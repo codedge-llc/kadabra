6604e314e184d35c8e514529fe2fc447de08bb37
Henry Popp <henry@codedge.io>
2017-01-15 13:38:20 -0600

    test: added some unit tests

--- test/kadabra_test.exs
+++ test/kadabra_test.exs
@@ -1,18 +1,70 @@
 defmodule KadabraTest do
   use ExUnit.Case
 
-  alias Kadabra.{Connection, Http2, Stream}
+  alias Kadabra.{Stream}
 
-  test "GET https://http2.golang.org/reqinfo" do
-    uri = 'http2.golang.org'
-    {:ok, pid} = Kadabra.open(uri, :https)
-    Kadabra.get(pid, "/reqinfo")
+  describe "GET"  do
+    test "https://http2.golang.org/reqinfo" do
+      uri = 'http2.golang.org'
+      {:ok, pid} = Kadabra.open(uri, :https)
+      Kadabra.get(pid, "/reqinfo")
 
-    assert_receive {:end_stream, %Stream{
-      id: 1,
-      headers: headers,
-      body: body,
-      status: 200
-    }}, 5_000
+      assert_receive {:end_stream, %Stream{
+        id: 1,
+        headers: _headers,
+        body: _body,
+        status: 200
+      }}, 5_000
+    end
+
+    test "https://http2.golang.org/redirect" do
+      uri = 'http2.golang.org'
+      {:ok, pid} = Kadabra.open(uri, :https)
+      Kadabra.get(pid, "/redirect")
+
+      expected_body = "<a href=\"/\">Found</a>.\n\n"
+      expected_status = 302
+
+      assert_receive {:end_stream, %Stream{
+        id: 1,
+        headers: _headers,
+        body: ^expected_body,
+        status: ^expected_status
+      }}, 5_000
+    end
+  end
+
+  describe "PUT" do
+    test "https://http2.golong.org/ECHO" do
+      uri = 'http2.golang.org'
+      {:ok, pid} = Kadabra.open(uri, :https)
+      payload = "test"
+      Kadabra.put(pid, "/ECHO", payload)
+
+      expected_body = String.upcase(payload)
+
+      assert_receive {:end_stream, %Stream{
+        id: 1,
+        headers: _headers,
+        body: ^expected_body,
+        status: 200
+      }}, 5_000
+    end
+
+    test "https://http2.golong.org/crc32" do
+      uri = 'http2.golang.org'
+      {:ok, pid} = Kadabra.open(uri, :https)
+      payload = "test"
+      Kadabra.put(pid, "/crc32", payload)
+
+      expected_body = "bytes=4, CRC32=d87f7e0c"
+
+      assert_receive {:end_stream, %Stream{
+        id: 1,
+        headers: _headers,
+        body: ^expected_body,
+        status: 200
+      }}, 5_000
+    end
   end
 end
