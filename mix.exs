53705cef4814c69311c6a24b7e44a18b9bcc1270
Henry Popp <henry@codedge.io>
2018-02-16 16:25:15 -0600

    fix: remove optional scribe dependency

--- mix.exs
+++ mix.exs
@@ -49,7 +49,6 @@ defmodule Kadabra.Mixfile do
   defp deps do
     [
       {:hpack, "~> 0.2.3", hex: :hpack_erl},
-      {:scribe, "~> 0.4", optional: true},
       {:ex_doc, "~> 0.14", only: :dev},
       {:dogma, "~> 0.1", only: :dev},
       {:excoveralls, "~> 0.7", only: :test},
